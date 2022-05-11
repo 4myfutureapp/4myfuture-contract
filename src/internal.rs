584a817dd955c88e875355d76ed19feafdb75ddf
Edward <edwards.svo@gmail.com>
2022-05-11 12:01:38 -0400

    implement create_proposal function

--- src/internal.rs
+++ src/internal.rs
@@ -1,1 +1,33 @@
 use crate::*;
+
+#[near_bindgen]
+impl Contract {
+    pub(crate) fn user_with_active_proposal(&self, account_id: AccountId) -> bool {
+        if !self.proposal_per_owner.contains_key(&account_id) {
+            return false 
+        }
+        let proposal = self.proposal_per_owner.get(&account_id).unwrap();
+        if proposal.status == 0 {
+            return true
+        }
+        false
+    }
+
+    pub(crate) fn add_proposal_to_storages(&mut self, proposal: Proposal, account_id: AccountId) {
+        self.proposal_by_id.insert(&proposal.id, &proposal);
+        self.proposal_metadata_by_id.insert(&proposal.id, &proposal.metadata);
+        self.proposal_per_owner.insert(&account_id, &proposal);
+    }
+
+    pub(crate) fn set_inactive_proposal(&mut self, proposal_id: ProposalId) {
+        let mut proposal = self.proposal_by_id.get(&proposal_id).unwrap();
+        proposal.status = 0;
+        self.proposal_by_id.insert(&proposal_id, &proposal);
+    }
+
+    pub(crate) fn set_complete_proposal(&mut self, proposal_id: ProposalId) {
+        let mut proposal = self.proposal_by_id.get(&proposal_id).unwrap();
+        proposal.status = 2;
+        self.proposal_by_id.insert(&proposal_id, &proposal);
+    }
+}
--- src/proposal.rs
+++ src/proposal.rs
@@ -1,1 +1,50 @@
 use crate::*;
+
+#[near_bindgen]
+impl Contract {
+    pub fn create_proposal(
+        &mut self,
+        title: String,
+        description: String,
+        goal: U128,
+        institution_link: String,
+        pensum_link: String,
+        finish_date: u64,
+        images: Vec<String>,
+    ) {
+        assert!(goal.0 > 0, "Invalid amount provided");
+        let user_id = env::signer_account_id().to_string();
+        assert!(
+            !self.user_with_active_proposal(user_id),
+            "User already have one active proposal"
+        );
+
+        let goal_in_yocto = U128(goal.0 * ONE_NEAR);
+        let index = i128::from(self.proposal_by_id.len() + 1);
+        let initial_storage_usage = env::storage_usage();
+
+        let proposal_metadata = ProposalMetadata {
+            title: title,
+            description: description,
+            goal: goal_in_yocto,
+            init_date: env::block_timestamp(),
+            finish_date: finish_date,
+            funds: 0,
+            images: images.clone(),
+            institution_link: institution_link,
+            pensum_link: pensum_link,
+        };
+
+        let proposal = Proposal {
+            id: index,
+            owner: env::signer_account_id().to_string(),
+            metadata: proposal_metadata,
+            image: images[0].clone(),
+            status: 0,
+        };
+
+        self.add_proposal_to_storages(proposal, env::signer_account_id());
+
+    //CREATE LOG FOR CREATE PROPOSAL
+    }
+}
